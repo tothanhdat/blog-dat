154e11908fa83424628c05fa7f189997e7427a30
tothanhdat <todat999@gmail.com>
2022-06-29 00:01:55 +0700

    update

--- app.js
+++ app.js
@@ -37,8 +37,9 @@ app.get('/about-me', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 const uri = 'mongodb://localhost/blogtodat';
+const uri_cloud = 'mongodb+srv://tothanhdat:Tothanhdat123@cluster0.dxjtsoc.mongodb.net/blogtodat?retryWrites=true&w=majority'
 
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.connect(uri_cloud, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
 });
