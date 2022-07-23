e94e70c875fd24775d91a6e22d0b542621bfb3d5
tothanhdat <todat999@gmail.com>
2022-07-23 14:27:59 +0700

    update topic

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,8 @@
 exports.CATEGORY = [
     {
         id: 2,
-        title: "Cuộc Sống",
-        image: "https://imgur.com/jJ5fEDg.jpg"
+        title: "Vu vơ",
+        image: "https://imgur.com/cEhyfoT.jpg"
     },
     {
         id: 3,
@@ -10,8 +10,13 @@ exports.CATEGORY = [
         image: "https://imgur.com/W7sh9w6.jpg"
     },
     {
-        id: 4,
-        title: "Du lịch",
-        image: "https://imgur.com/qeIoZ2H.jpg"
+        id: 5,
+        title: "IT-BA",
+        image: "https://imgur.com/G9zEmIR.jpg"
     },
+    // {
+    //     id: 4,
+    //     title: "Du lịch",
+    //     image: "https://imgur.com/qeIoZ2H.jpg"
+    // },
 ]
