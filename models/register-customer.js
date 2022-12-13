cd8e281c6fc9157d0d5cf9e25d189fa8b190b85f
tothanhdat <todat999@gmail.com>
2022-12-13 22:35:17 +0700

    create google analyst

--- models/register-customer.js
+++ models/register-customer.js
@@ -2,17 +2,20 @@ const ObjectID = require('mongoose').Types.ObjectId;
 const REGISTER_CUSTOMER_COLL = require('../database/register-customer-coll');
 const nodemailer = require("nodemailer");
 const { OAuth2Client  } = require('google-auth-library');
+// const TelegramBot = require('node-telegram-bot-api');
 
 require('dotenv').config();
 
-const CLIENT_ID = process.env.CLIENT_EMAIL_ID
-const CLIENT_SECRET = process.env.CLIENT_EMAIL_SECRET
+const CLIENT_ID = process.env.CLIENT_EMAIL_ID;
+const CLIENT_SECRET = process.env.CLIENT_EMAIL_SECRET;
 // const REDIRECT_URI = process.env.REDIRECT_URI
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN
-const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL;
+const TOKEN_TELEGRAM = '5920362563:AAErtHRja5JWUfqWACpWZwqg1QOacJ-Hslg';
 
-// const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-// oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
+// const token = TOKEN_TELEGRAM;
+// const bot = new TelegramBot(token, {polling: true});
+
 
 const myOAuth2Client = new OAuth2Client(
     CLIENT_ID,
@@ -38,6 +41,26 @@ module.exports = class REGISTER_CUSTOMER extends REGISTER_CUSTOMER_COLL {
 
                 // if(accountExisted)
                 //     return resolve({ error: true, code: 400, message: 'Bạn đã đăng ký email này' });
+
+                // bot.onText(/\/echo (.+)/, (msg, match) => {
+                
+                  
+                //     const chatId = msg.chat.id;
+                //     const resp = match[1]; // the captured "whatever"
+                  
+                //     bot.sendMessage(chatId, resp);
+                // });
+
+                // console.log("==============2==========");
+
+                // bot.on('message', (msg) => {
+                //     const chatId = msg.chat.id;
+                //     console.log({ chatId });
+                    
+                //     bot.sendMessage(chatId, `${name} (${email}) đã để lại bình luận \n${questionContent}`);
+                // });
+
+                // console.log("==============3==========");
 
                 let infoAfterInsert = new REGISTER_CUSTOMER({ name, email, questionContent });
                 let saveDataInsert = await infoAfterInsert.save();
