83c50e9e5ec44b01a0a233f72307f1fed6c8cdbb
tothanhdat <todat999@gmail.com>
2023-02-13 01:21:27 +0700

    add post

--- constants/config.js
+++ constants/config.js
@@ -4,15 +4,20 @@ exports.CATEGORY = [
         title: "Vu vơ",
         image: "https://imgur.com/cEhyfoT.jpg"
     },
-    {
-        id: 3,
-        title: "Travel",
-        image: "https://i.imgur.com/YDWhitN.jpg"
-    },
+    // {
+    //     id: 3,
+    //     title: "Travel",
+    //     image: "https://i.imgur.com/YDWhitN.jpg"
+    // },
     {
         id: 5,
         title: "IT & BA",
         image: "https://imgur.com/G9zEmIR.jpg"
+    },
+        {
+        id: 6,
+        title: "Tutorial",
+        image: "https://dytvr9ot2sszz.cloudfront.net/wp-content/uploads/2019/05/1200x628_logstash-tutorial-min.jpg"
     },
     // {
     //     id: 4,
